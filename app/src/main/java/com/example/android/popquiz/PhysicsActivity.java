e36987845319c258783a523bcc50364f0243266d
Mfundo07 <mfundombokoma07@gmail.com>
2017-06-06 14:26:05 +0200

    commit

--- app/src/main/java/com/example/android/popquiz/PhysicsActivity.java
+++ app/src/main/java/com/example/android/popquiz/PhysicsActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.popquiz;
 
+import android.animation.LayoutTransition;
 import android.graphics.Color;
 import android.support.annotation.IdRes;
+import android.support.transition.Fade;
+import android.support.transition.Scene;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
@@ -16,6 +20,7 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 import static android.R.attr.button;
+import static android.R.attr.thickness;
 import static com.example.android.popquiz.R.id.radio_button2_a1;
 
 public class PhysicsActivity extends AppCompatActivity {
@@ -25,6 +30,7 @@ public class PhysicsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physics);
+
 
     }
 }
