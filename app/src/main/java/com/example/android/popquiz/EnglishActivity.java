8b2a24a7cca1d4d25698f254ed72c1f345877c1f
Mfundo07 <mfundombokoma07@gmail.com>
2017-06-06 13:03:59 +0200

    commit

--- app/src/main/java/com/example/android/popquiz/EnglishActivity.java
+++ app/src/main/java/com/example/android/popquiz/EnglishActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.popquiz;
 
+import android.graphics.Color;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static com.example.android.popquiz.R.id.radio_button2_a1;
 
 public class EnglishActivity extends AppCompatActivity {
+
+    int scoreCard = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english);
+
+
+
     }
 }
